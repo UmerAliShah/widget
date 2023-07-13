5c2ee1daf0e142b013de4154fd68f514240989f4
UmerAliShah <muhammadumeralishah@gmail.com>
2023-07-13 12:19:18 -0700

    added widget

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, FormControl, InputGroup } from "react-bootstrap";
 
 const WidgetPopup = () => {
   const [showModal, setShowModal] = useState(false);
+  const [donationData, setDonationData] = useState([]);
+  console.log(donationData, 'saving in widget')
+  const organizations = [
+    { name: "Organization 1" },
+    { name: "Organization 2" },
+    { name: "Organization 3" },
+    { name: "WE are needy" },
+  ];
 
   const handleButtonClick = () => {
     setShowModal(!showModal);
@@ -11,8 +19,13 @@ const WidgetPopup = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
-  const handleAmountClick = (am) => {
-    console.log(am, "am");
+
+  const handleAmountClick = (org, amount) => {
+    const donation = { organization: org.name, amount: amount };
+    setDonationData([...donationData, donation]);
+    console.log(donation.organization, donation.amount);
+
+    window.parent.postMessage({ donationData: [...donationData, donation] }, "*");
   };
 
   return (
@@ -30,15 +43,31 @@ const WidgetPopup = () => {
           <Modal.Title>Widget Popup</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Button value="10" onClick={(e) => handleAmountClick(e.target.value)}>
-            10
-          </Button>
-          <Button value="20" onClick={(e) => handleAmountClick(e.target.value)}>
-            20
-          </Button>
-          <Button value="30" onClick={(e) => handleAmountClick(e.target.value)}>
-            30
-          </Button>
+          <div style={{ maxHeight: "200px", overflowY: "auto" }}>
+            {organizations.map((org, index) => (
+              <div key={index}>
+                <h5>{org.name}</h5>
+                <Button onClick={() => handleAmountClick(org, 10)}>Donate $10</Button>
+                <Button onClick={() => handleAmountClick(org, 20)}>Donate $20</Button>
+                <Button onClick={() => handleAmountClick(org, 30)}>Donate $30</Button>
+                <InputGroup>
+                  <FormControl
+                    type="number"
+                    placeholder="Enter custom amount"
+                    onChange={(e) => {
+                      const customAmount = parseInt(e.target.value);
+                      if (!isNaN(customAmount)) {
+                        handleAmountClick(org, customAmount);
+                      }
+                    }}
+                  />
+                  <Button variant="secondary" onClick={() => handleAmountClick(org, 0)}>
+                    Donate Custom Amount
+                  </Button>
+                </InputGroup>
+              </div>
+            ))}
+          </div>
           <p>This is the content of the widget popup.</p>
         </Modal.Body>
         <Modal.Footer>
