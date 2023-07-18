7f3c40dbe42b74475157190682d2a62649001166
Hasnat <hasnat@fabtechsol.com>
2023-07-18 23:31:29 +0500

    minor design changed

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Button, Modal, FormControl, InputGroup } from "react-bootstrap";
-import img_logo from '../images/heart_logo.png';
-import logo from '../images/logo.png';
-import logo2 from '../images/logo2.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import img_logo from "../images/heart_logo.png";
+import logo from "../images/logo.png";
+import logo2 from "../images/logo2.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const WidgetPopup = () => {
   const settings = {
@@ -29,24 +29,27 @@ const WidgetPopup = () => {
     }
   }, []);
 
-  console.log(donationData, 'saving in widget');
+  console.log(donationData, "saving in widget");
 
   const organizations = [
-    { 
+    {
       name: "Donation To Food Wise",
-      description:"Donate and Help Companies Redistributes Surplus Food to People In Need",
-      logo:img_logo
+      description:
+        "Donate and Help Companies Redistributes Surplus Food to People In Need",
+      logo: img_logo,
     },
-    { 
+    {
       name: "Donation Sahara life",
-      description:"Donate and Help Companies Redistributes Surplus Medicines to People In Need",
-      logo:img_logo
+      description:
+        "Donate and Help Companies Redistributes Surplus Medicines to People In Need",
+      logo: img_logo,
     },
-    { 
+    {
       name: "Donation Eidi Center",
-      description:"Donate and Help Companies Redistributes Surplus cloths to People In Need",
-      logo:logo
-    }
+      description:
+        "Donate and Help Companies Redistributes Surplus cloths to People In Need",
+      logo: logo,
+    },
   ];
 
   const handleButtonClick = () => {
@@ -61,7 +64,9 @@ const WidgetPopup = () => {
     setSelectedAmount(amount);
 
     const donation = { organization: org.name, amount: amount };
-    const existingDonationIndex = donationData.findIndex(item => item.organization === org.name);
+    const existingDonationIndex = donationData.findIndex(
+      (item) => item.organization === org.name
+    );
 
     if (existingDonationIndex !== -1) {
       const updatedDonationData = [...donationData];
@@ -72,13 +77,19 @@ const WidgetPopup = () => {
     }
 
     console.log(donation.organization, donation.amount);
-    window.parent.postMessage({ donationData: [...donationData, donation] }, "*");
+    window.parent.postMessage(
+      { donationData: [...donationData, donation] },
+      "*"
+    );
   };
 
   const handleDonation = () => {
     // Handle the donation logic here
     // ...
-    localStorage.setItem("donationData", JSON.stringify([...donationData, donationData]));
+    localStorage.setItem(
+      "donationData",
+      JSON.stringify([...donationData, donationData])
+    );
 
     // Clear the selected amount
     setSelectedAmount(0);
@@ -95,14 +106,22 @@ const WidgetPopup = () => {
 
   return (
     <div className="widget">
-      <div className="widget-button" style={{backgroundColor:"transparent"}}>
-        <Button className="btn text-white" onClick={handleButtonClick}>Open Widget</Button>
+      <div className="widget-button" style={{ backgroundColor: "transparent" }}>
+        <Button className="btn text-white" onClick={handleButtonClick}>
+          Open Widget
+        </Button>
       </div>
       <Modal
         show={showModal}
         onHide={handleCloseModal}
         className="widget-popup"
-        style={{ color: "black", backgroundColor:"transparent" }}
+        style={{
+          width:  "100%",
+          position: "fixed",
+          bottom: "10px",
+          right: "20px",
+          zIndex: "9999",
+        }}
       >
         <Modal.Header closeButton>
           <Modal.Title className="text-dark">Kind Heart</Modal.Title>
@@ -111,29 +130,41 @@ const WidgetPopup = () => {
           <div>
             <Slider {...settings}>
               {organizations.map((org, index) => {
-                const isAmountSelected = selectedAmount > 0 && org.name === donationData.find(item => item.organization === org.name)?.organization;
+                const isAmountSelected =
+                  selectedAmount > 0 &&
+                  org.name ===
+                    donationData.find((item) => item.organization === org.name)
+                      ?.organization;
                 return (
                   <div className="main-slide" key={index}>
                     <h5 className="text-dark">{org.name}</h5>
                     <div className="com-info">
-                      <a href="#"><img src={org.logo} alt="" /></a>
+                      <a href="#">
+                        <img src={org.logo} alt="" />
+                      </a>
                       <p className="text-dark">{org.description}</p>
                     </div>
                     <div className="Actions">
                       <Button
-                        className={`btn-1 ${selectedAmount === 10 ? 'active' : ''}`}
+                        className={`btn-1 ${
+                          selectedAmount === 10 ? "active" : ""
+                        }`}
                         onClick={() => handleAmountClick(org, 10)}
                       >
                         10
                       </Button>
                       <Button
-                        className={`btn-2 ${selectedAmount === 20 ? 'active' : ''}`}
+                        className={`btn-2 ${
+                          selectedAmount === 20 ? "active" : ""
+                        }`}
                         onClick={() => handleAmountClick(org, 20)}
                       >
                         20
                       </Button>
                       <Button
-                        className={`btn-3 ${selectedAmount === 30 ? 'active' : ''}`}
+                        className={`btn-3 ${
+                          selectedAmount === 30 ? "active" : ""
+                        }`}
                         onClick={() => handleAmountClick(org, 30)}
                       >
                         30
